e60d8f149954d14683a7393065e4a065e8d8427b
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-08-02 20:47:33 +0530

    moved some functions to utils.py

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import sys
 import pywikibot
 import waybackpy
 import requests
+import logging
 import langdetect as lang
 from datetime import datetime
 from pywikibot import pagegenerators
 
-
-def uploader(filename, link=True):
-    """User that uploaded the file."""
-    history = (pywikibot.Page(SITE, filename)).revisions(reverse=True, total=1)
-    for info in history:
-        username = (info.user)
-    if not history:
-        return "Unknown"
-    if link:
-        return "[[User:%s|%s]]" % (username, username)
-    return username
+from utils import (
+uploader,
+last_edit_time,
+informatdate,
+IsMarkedForDeletion,
+DetectSite,
+check_channel,
+OwnWork,
+display_video_info,
+out,
+)
 
 def dump_file(filename):
     """Dump files if review not possible for multiple times."""
@@ -31,161 +32,6 @@ def dump_file(filename):
         commit(dump2_pagetext,(dump2_pagetext + "\n#[[:" + filename + "]]"),pywikibot.Page(SITE,"User:YouTubeReviewBot/dump2",),"dumped [[%s]]" % filename ,)
     else:
         commit(dump1_pagetext,(dump1_pagetext + "\n#[[:" + filename + "]]"),pywikibot.Page(SITE,"User:YouTubeReviewBot/dump1",),"dumped [[%s]]" % filename ,)
-
-def upload_date(filename):
-    """Upload date of the file."""
-    for info in (pywikibot.Page(SITE, filename)).revisions(reverse=True, total=1):
-        return datetime.strptime(str(info.timestamp), "%Y-%m-%dT%H:%M:%SZ")
-
-def last_edit_time(file_name):
-    """Recent most editor for file."""
-    for info in (pywikibot.Page(SITE, file_name)).revisions(reverse=False, total=1):
-        return datetime.strptime(str(info.timestamp), "%Y-%m-%dT%H:%M:%SZ")
-
-def informatdate():
-    """Current date in yyyy-mm-dd format."""
-    return (datetime.utcnow()).strftime('%Y-%m-%d')
-
-def AutoFill(site, text, source, author, VideoTitle, uploaddate, description, Replace_nld):
-    """Auto fills empty information template parameters."""
-    if site == "YouTube":
-        License = "{{YouTube CC-BY|%s}}" % author
-
-        # Handle cases where there's no description at all on YouTube
-        if description.isspace() or not description:
-            description = VideoTitle
-
-    elif site == "Vimeo": #Not Implemented yet, not required yet
-        uploaddate = ""
-        description = ""
-
-    #remove scheme from urls
-    description = re.sub('https?://', '', description).strip()
-
-    if not re.search(r"\|description=(.*)",text).group(1):
-        description = "{{%s|%s}}" % (lang.detect(description), description)
-        text = text.replace("|description=","|description=%s" % description ,1)
-
-    if uploaddate:
-        text = re.sub("\|date=.*", "|date=%s" % uploaddate, text)
-
-    text = re.sub("\|source=.*", "|source=%s" % source, text)
-    text = re.sub("\|author=.*", "|author=%s" % author, text)
-
-    if Replace_nld:
-        text = re.sub("{{No license since.*?}}", "%s" % License, text, re.IGNORECASE)
-        text = re.sub("{{(?:|\s)[Yy]ou(?:|\s)[Tt]ube(?:\|.*?|)(?:|\s)}}", "%s" % License, text)
-        text = re.sub("{{(?:|\s)[Yy]ou(?:|\s)[Tt]ube(?:|\s*?)(?:[Cc]{2}-[Bb][Yy]).*?}}", "%s" % License, text)
-    return text
-
-def IsMarkedForDeletion(pagetext):
-    """Determine if the file is marked for deletion."""
-    LowerCasePageText = pagetext.lower()
-    if (
-        (LowerCasePageText.find('{{No permission since') != -1) or
-        (LowerCasePageText.find('{{delete') != -1) or
-        (LowerCasePageText.find('{{copyvio') != -1) or
-        (LowerCasePageText.find('{{speedy') != -1)
-        ):
-            return True
-
-def DetectSite(source_area):
-    """Identify the source website of the file."""
-    if (source_area.find('{{from vimeo') != -1):
-        return "Vimeo"
-    elif (source_area.find('{{from youtube') != -1):
-        return "YouTube"
-    elif (source_area.find('videowiki.wmflabs.org') != -1):
-        return "VideoWiki"
-    elif (source_area.find('flickr.com/photos') != -1):
-        return "Flickr"
-    elif (source_area.find('vimeo.com') != -1):
-        return "Vimeo"
-    elif (source_area.find('youtube.com') != -1):
-        return "YouTube"
-
-def archived_url(SourceURL):
-    """Get a real-time archived url of the source url."""
-    archive_url = None
-    status = "Wait"
-    iters = 0
-    while status == "Wait":
-        iters += 1
-        try:
-            archive_url = waybackpy.Url(SourceURL, "User:YouTubeReviewBot on wikimedia commons").save()
-            status = "Done"
-        except Exception as e:
-            out(
-                e,
-                color="red",
-                )
-        if iters > 5:
-            status = "Stop"
-    return archive_url
-
-def oldest_ia_page(archive_url):
-    url = re.search(r"(?:[0-9])\/(.*)", archive_url).group(1)
-    url = ("https://archive.org/wayback/available?url={url}&timestamp=1998").format(url=url)
-    target = waybackpy.Url(url, "User:YouTubeReviewBot on wikimedia commons")
-    return target.get(target.oldest())
-
-def archived_webpage(archive_url):
-    """Get the source code of the archived webpage."""
-    webpage = None
-    status = "Wait"
-    iters = 0
-    while status == "Wait":
-        iters += 1
-        try:
-            webpage = waybackpy.Url(archive_url, "User:YouTubeReviewBot on wikimedia commons").get()
-            status = "Done"
-        except Exception as e:
-            out(
-                e,
-                color="red",
-                )
-        if iters > 5:
-            status = "Stop"
-    error301 = "Got an HTTP 301 response at crawl time"
-    if error301 in webpage and webpage is not None:
-        out("%s - try to get oldest archived_snapshots" % error301 ,color="red",)
-        try:
-            webpage = oldest_ia_page(archive_url)
-        except Exception as e:
-            out(
-                e,
-                color="red",
-                )
-    return webpage
-
-def check_channel(ChannelId):
-    """Check if the channel is trusted or bad."""
-    if ChannelId in (pywikibot.Page(SITE, "User:YouTubeReviewBot/Trusted")).get(get_redirect=True, force=True):
-        return "Trusted"
-    elif ChannelId in (pywikibot.Page(SITE, "User:YouTubeReviewBot/bad-authors")).get(get_redirect=True, force=True):
-        return "Bad"
-    return "Normal"
-
-def display_video_info(VideoId,ChannelId,VideoTitle,ArchiveUrl,ChannelName="Not Applicable"):
-    out(
-        str(
-            "video Id : " + VideoId +
-            "\nChannel Name : " + ChannelName +
-            "\nChannel Id : " + ChannelId +
-            "\nVideo Title : " + VideoTitle +
-            "\nArchive : " + ArchiveUrl +
-            "\nDate : " + informatdate()),
-            color="white",
-            )
-
-def OwnWork(pagetext):
-    """Check if own work by uploader."""
-    LowerCasePageText = pagetext.lower()
-    if (LowerCasePageText.find('{{own}}') != -1):
-        return True
-    elif (LowerCasePageText.find('own work') != -1):
-        return True
-    return False
 
 def commit(old_text, new_text, page, summary):
     """Show diff and submit text to the wiki server."""
@@ -216,8 +62,8 @@ def commit(old_text, new_text, page, summary):
         page.put(
             new_text,
             summary=summary,
-            watchArticle=True,
-            minorEdit=False,
+            watch=True,
+            minor=False,
             )
 
     elif choice in esc:
@@ -226,19 +72,37 @@ def commit(old_text, new_text, page, summary):
     else:
         pass
 
-def out(text, newline=True, date=False, color=None):
-    """Just output some text to the consoloe or log."""
-    if color:
-        text = "\03{%s}%s\03{default}" % (color, text)
-    dstr = (
-        "%s: " % datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-        if date
-        else ""
-    )
-    pywikibot.stdout(
-        "%s%s" % (dstr, text),
-        newline=newline,
-        )
+def AutoFill(site, text, source, author, VideoTitle, uploaddate, description, Replace_nld):
+    """Auto fills empty information template parameters."""
+    if site == "YouTube":
+        License = "{{YouTube CC-BY|%s}}" % author
+
+        # Handle cases where there's no description at all on YouTube
+        if description.isspace() or not description:
+            description = VideoTitle
+
+    if site == "Vimeo": #Not Implemented yet, not required yet
+        uploaddate = ""
+        description = ""
+
+    #remove scheme from urls
+    description = re.sub('https?://', '', description).strip()
+
+    if not re.search(r"\|description=(.*)",text):
+        description = "{{%s|%s}}" % (lang.detect(description), description)
+        text = text.replace("|description=","|description=%s" % description ,1)
+
+    if uploaddate:
+        text = re.sub("\|date=.*", "|date=%s" % uploaddate, text)
+
+    text = re.sub("\|source=.*", "|source=%s" % source, text)
+    text = re.sub("\|author=.*", "|author=%s" % author, text)
+
+    if Replace_nld:
+        text = re.sub("{{No license since.*?}}", "%s" % License, text, re.IGNORECASE)
+        text = re.sub("{{(?:|\s)[Yy]ou(?:|\s)[Tt]ube(?:\|.*?|)(?:|\s)}}", "%s" % License, text)
+        text = re.sub("{{(?:|\s)[Yy]ou(?:|\s)[Tt]ube(?:|\s*?)(?:[Cc]{2}-[Bb][Yy]).*?}}", "%s" % License, text)
+    return text
 
 def checkfiles():
     category = pywikibot.Category(
@@ -265,26 +129,15 @@ def checkfiles():
                 )
 
         if filename in dumpHell:
-            out(
-                "IGNORE - File dumped for 3rd time.",
-                color='red',
-                )
+            out("IGNORE - File dumped for 3rd time.", color='red')
             continue
 
-
         if (datetime.utcnow()-last_edit_time(filename)).days > 30:
-            out(
-                "File is not edited for a long time 1 months, will not process it.",
-                color='red',
-                )
+            out("''%s' is not edited for more than a 1 month, skipping it." % filename, color='red')
             dump_file(filename)
             continue
 
-        page = pywikibot.Page(
-            SITE,
-            filename,
-            )
-
+        page = pywikibot.Page(SITE, filename)
         old_text = pagetext = page.get()
 
         try:
@@ -297,63 +150,42 @@ def checkfiles():
 
         Identified_site = DetectSite((source_area.lower()))
 
-        out(
-            "Identified as %s" % Identified_site,
-            color="yellow",
-            )
+        out("A file from %s." % Identified_site, color="yellow")
 
         if not Identified_site:
             continue
 
         if IsMarkedForDeletion(pagetext) is True:
-            out(
-                "IGNORE - File is marked for deletion",
-                color='red',
-                )
+            out("IGNORE - File is marked for deletion", color='red')
             continue
 
         elif Identified_site == "VideoWiki":
             new_text = re.sub(RegexOfLicenseReviewTemplate, "" , old_text)
-            EditSummary = "@%s Removing licenseReview Template, not required for video-wiki files beacuse all constituent files are from commons." % uploader(
-                filename,
-                link=True,
-                )
+            EditSummary = "@%s removing license Review template, not required for video-wiki files\
+            because all constituent files are from commons." % uploader(filename, link=True)
+
             try:
                 commit(old_text, new_text, page, EditSummary)
             except pywikibot.LockedPage as error:
-                out(
-                    "Page is locked '%s'." % error,
-                    color='red',
-                    )
+                out("Page is locked '%s'." % error, color='red')
                 continue
 
         elif OwnWork(pagetext):
             new_text = re.sub(RegexOfLicenseReviewTemplate, "" , old_text)
-            EditSummary = "@%s Removing licenseReview Template, not required for ownwork." % uploader(filename,link=True)
+            EditSummary = "@%s removing license Review template, not required for ownwork." % uploader(filename,link=True)
             try:
                 commit(old_text, new_text, page, EditSummary)
             except pywikibot.LockedPage as error:
-                out(
-                    "Page is locked '%s'." % error,
-                    color='red',
-                    )
+                out("Page is locked '%s'." % error, color='red')
                 continue
 
         elif Identified_site == "Flickr":
             new_text = re.sub(RegexOfLicenseReviewTemplate, "{{FlickrReview}}" , old_text)
             EditSummary = "@%s Marking for flickr review, file's added to [[Category:Flickr videos review needed]]." % uploader(filename,link=True)
             try:
-                commit(
-                    old_text,
-                    new_text,
-                    page,
-                    EditSummary,
-                    )
+                commit(old_text, new_text, page, EditSummary)
             except pywikibot.LockedPage as error:
-                out(
-                    "Page is locked '%s'." % error,
-                    color='red',
-                    )
+                out("Page is locked '%s'." % error, color='red')
                 continue
 
         elif Identified_site == "Vimeo":
@@ -363,38 +195,29 @@ def checkfiles():
                 try:
                     VimeoVideoId = re.search(r"vimeo\.com\/((?:[0-9_]+))", source_area).group(1)
                 except AttributeError:
-                    out(
-                        "PARSING FAILED - Can't get VimeoVideoId",
-                        color='red',
-                        )
+                    out("PARSING FAILED - Can't get VimeoVideoId", color='red')
                     continue
+
             SourceURL = "https://vimeo.com/%s" % VimeoVideoId
+            user_agent = "User:YouTubeReviewBot on wikimedia commons"
 
             try:
-                if archived_url(SourceURL) is not None:
-                    archive_url = archived_url(SourceURL)
-                else:
-                    out(
-                        "WAYBACK FAILED - Can't get archive_url",
-                        color='red',
-                        )
+                archive_url = waybackpy.Url(SourceURL, user_agent).newest()
+            except Exception:
+                try:
+                    archive_url = waybackpy.Url(SourceURL, user_agent).save()
+                except Exception as e:
                     dump_file(filename)
+                    out(e, color="red")
                     continue
 
-                if archived_webpage(archive_url) is None:
-                    out(
-                        "WAYBACK FAILED - Can't get webpage",
-                        color='red',
-                        )
-                    dump_file(filename)
-                    continue
-
-                else:
-                    webpage = archived_webpage(archive_url)
+            try:
+                webpage = waybackpy.Url(SourceURL, user_agent).get()
             except Exception as e:
                 out(e, color="red")
                 dump_file(filename)
                 continue
+
 
             # Try to get the ChannelID
             try:
@@ -443,22 +266,16 @@ def checkfiles():
                     'cc0',
                     ]
                 if licensesP1 not in Allowedlicenses:
-                    out(
-                        "The file is licensed under %s, but it's not allowed on commons" % VimeoLicense,
-                        color="red",
-                        )
+                    out("The file is licensed under %s, but it's not allowed on commons" % VimeoLicense, color="red")
                     dump_file(filename)
                     continue
-                else:pass
             else:
-                out(
-                    "Creative commons Not found - File is not licensed under any type of creative commons license including CC-NC/ND",
-                    color='red'
-                    )
+                out("Creative commons Not found - File is not licensed under any type of creative commons\
+                license including CC-NC/ND", color='red')
                 dump_file(filename)
-                continue
+                continues
 
-            TAGS = '{{VimeoReview|id=%s|title=%s|license=%s|ChannelID=%s|archive=%s|date=%s}}' % (
+            TAGS = "{{VimeoReview|id=%s|title=%s|license=%s|ChannelID=%s|archive=%s|date=%s}}" % (
                 VimeoVideoId,
                 VimeoVideoTitle,
                 VimeoLicense,
@@ -470,18 +287,10 @@ def checkfiles():
             #Out puts some basic info about the video.
             display_video_info(VimeoVideoId,VimeoChannelId,VimeoVideoTitle,archive_url)
 
-            new_text = re.sub(
-                RegexOfLicenseReviewTemplate,
-                TAGS,
-                old_text
-                )
+            new_text = re.sub(RegexOfLicenseReviewTemplate, TAGS, old_text)
 
-            EditSummary = "LR Passed, %s , by %s under terms of %s at https://vimeo.com/%s (Archived - WayBack Machine)" % (
-                VimeoVideoTitle,
-                VimeoChannelId,
-                VimeoLicense,
-                VimeoVideoId,
-                )
+            EditSummary = "LR Passed, %s , by %s under terms of %s at https://vimeo.com/%s (Archived\
+            - WayBack Machine)" % (VimeoVideoTitle, VimeoChannelId, VimeoLicense, VimeoVideoId)
 
             try:
                 commit(
@@ -491,10 +300,7 @@ def checkfiles():
                     EditSummary
                     )
             except pywikibot.LockedPage as error:
-                out(
-                    "Page is locked '%s'." % error,
-                    color='red'
-                    )
+                out("Page is locked '%s'." % error, color='red')
                 continue
 
         elif Identified_site == "YouTube":
@@ -553,7 +359,8 @@ def checkfiles():
                 out("Not found upload_date in %s" % archive_url)
 
             if total_none >= 3:
-                out("Can not parse any data from %s. Ignoring file." % archive_url)
+                out("Can not parse any data from %s. Dumping file." % archive_url)
+                dump_file(filename)
                 continue
 
             TAGS = str(
@@ -669,15 +476,14 @@ def main(*args):
             "-auto",
             "-dry",
             ]:
-                out(
-                    "Warning - unknown argument '%s' aborting, use -auto for automatic review or -dry to test and not submit the edits. see -help for pywikibot help" % arg,
-                    color="lightred",
-                    )
+                out("Warning - unknown argument '%s' aborting, use -auto for automatic review\
+                or -dry to test and not submit the edits. see -help for pywikibot help" % arg, color="red")
                 sys.exit(0)
+
     try:
         checkfiles()
     except Exception as e:
-        out(e, color="red")
+        logging.error(e, exc_info=True)
 
 
 if __name__ == "__main__":
