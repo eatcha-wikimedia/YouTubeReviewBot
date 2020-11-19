a168a29b18bf51eab27c0a1c9f2dc992daa29faa
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-11-19 12:25:28 +0530

    create separate function for extracting different youtube video attributes

--- youtube.py
+++ youtube.py
@@ -2,7 +2,8 @@ import waybackpy
 from datetime import datetime
 import re
 import html as htm
-
+import requests
+from utils import out
 
 
 def clean_html(html):
@@ -41,119 +42,159 @@ def get_video_description(source_code):
             video_description = match.group(1)
         else:
             video_description = None
-
-
     return clean_html(video_description)
 
 def get_archive(url, user_agent):
     obj = waybackpy.Url(url, user_agent)
     archive_url = None
 
-    try:
-        archive_url = obj.archive_url
-    except waybackpy.exceptions.WaybackError as e:
-        # print(e)
+    archives_count = obj.total_archives()
+
+    if archives_count > 5:
+
         try:
-            archive_url = obj.save()
-        except waybackpy.exceptions.WaybackError as e:
-            print(e)
+            archive_url = obj.oldest()
+        except waybackpy.exceptions.WaybackError as a:
+            try:
+                archive_url = obj.newest()
+            except waybackpy.exceptions.WaybackError as e:
+                if not a:
+                    a = ""
+                out(a + "\n" + e, color="red")
+    else:
+
+        try:
+            archive_url = obj.oldest()
+        except waybackpy.exceptions.WaybackError as a:
+            try:
+                archive_url = obj.save()
+            except waybackpy.exceptions.WaybackError as e:
+                if not a:
+                    a = ""
+                out(a + "\n" + e, color = "red")
 
     return archive_url
 
-def ytdata(video_id, user_agent):
-    url = "https://www.youtube.com/watch?v=%s" % video_id
-    archive_url = get_archive(url, user_agent)
-
-
-    if archive_url:
-
-        while True:
-            try:
-                source_code = waybackpy.Url(archive_url, user_agent).get()
-                break
-            except:
-                pass
-
-
-
-        upload_date = re.search(r"<strong class=\"watch-time-text\">(?:Published on|Premiered) ([A-Za-z]*?) ([0-9]{1,2}), ([0-9]{2,4})</strong>", source_code)
+def get_upload_date(source_code):
+    upload_date = re.search(r"<strong class=\"watch-time-text\">(?:Published on|Premiered) ([A-Za-z]*?) ([0-9]{1,2}), ([0-9]{2,4})</strong>", source_code)
+    if upload_date:
+        upload_date = datetime.strptime(("%s %s %s" % (upload_date.group(2), upload_date.group(1), upload_date.group(3))), "%d %b %Y").date()
+    else:
+        upload_date = re.search(r"\"dateText\":{\"simpleText\":\"([A-Za-z]*?) ([0-9]{1,2}), ([0-9]{2,4})\"}", source_code)
         if upload_date:
-            upload_date = datetime.strptime(("%s %s %s" % (upload_date.group(2), upload_date.group(1), upload_date.group(3))), "%d %b %Y").date()
+            upload_date = str(datetime.strptime(("%s %s %s" % (upload_date.group(2), upload_date.group(1), upload_date.group(3))), "%d %b %Y").date())
         else:
-            upload_date = re.search(r"\"dateText\":{\"simpleText\":\"([A-Za-z]*?) ([0-9]{1,2}), ([0-9]{2,4})\"}", source_code)
+            upload_date = re.search(r"\"uploadDate\":\"([0-9]{2,4})-([0-9]{1,2})-([0-9]{1,2})\"", source_code)
             if upload_date:
-                upload_date = str(datetime.strptime(("%s %s %s" % (upload_date.group(2), upload_date.group(1), upload_date.group(3))), "%d %b %Y").date())
+                upload_date = str(datetime.strptime(("%s %s %s" % (upload_date.group(1), upload_date.group(2), upload_date.group(3))), "%Y %m %d").date())
             else:
-                upload_date = re.search(r"\"uploadDate\":\"([0-9]{2,4})-([0-9]{1,2})-([0-9]{1,2})\"", source_code)
-                if upload_date:
-                    upload_date = str(datetime.strptime(("%s %s %s" % (upload_date.group(1), upload_date.group(2), upload_date.group(3))), "%Y %m %d").date())
-                else:
-                    upload_date = None
+                upload_date = None
+    return upload_date
 
+def get_youtube_channel_id(source_code):
+    YouTubeChannelIdRegex1 = r"data-channel-external-id=\"(.{0,30})\""
+    YouTubeChannelIdRegex2 = r"[\"']externalChannelId[\"']:[\"']([a-zA-Z0-9_-]{0,25})[\"']"
+    YouTubeChannelIdRegex3 = r"\"channelId\":\"(UC[^\",]*?)\","
 
-        video_description = get_video_description(source_code)
-
-
-        YouTubeChannelIdRegex1 = r"data-channel-external-id=\"(.{0,30})\""
-        YouTubeChannelIdRegex2 = r"[\"']externalChannelId[\"']:[\"']([a-zA-Z0-9_-]{0,25})[\"']"
+    try:
+        YouTubeChannelId = re.search(YouTubeChannelIdRegex1, source_code).group(1)
+    except AttributeError:
         try:
-            YouTubeChannelId = re.search(YouTubeChannelIdRegex1, source_code).group(1)
+            YouTubeChannelId = re.search(YouTubeChannelIdRegex2, source_code).group(1)
         except AttributeError:
             try:
-                YouTubeChannelId = re.search(YouTubeChannelIdRegex2, source_code).group(1)
+                YouTubeChannelId = re.search(YouTubeChannelIdRegex3, source_code).group(1)
             except AttributeError:
                 YouTubeChannelId = None
+    return YouTubeChannelId
 
+def get_youtube_channel_name(source_code):
+    YouTubeChannelNameRegex1 = r"\\\",\\\"author\\\":\\\"(.{1,50})\\\",\\\""
+    YouTubeChannelNameRegex2 = r"\"ownerChannelName\\\":\\\"(.{1,50})\\\","
+    YouTubeChannelNameRegex3 = r"Unsubscribe from ([^<{]*?)\?"
+    YouTubeChannelNameRegex4 = r"\"ownerChannelName\":\"(.*?)\","
+    YouTubeChannelNameRegex5 = r"<span class=\"g-hovercard\" data-name=\"relmfu\" data-ytid=\"(?:[^\"]*?)\">(.*?)</span>"
+    YouTubeChannelNameRegex6 = r"\",\"author\":\"(.{1,50})\",\""
 
-        YouTubeChannelNameRegex1 = r"\\\",\\\"author\\\":\\\"(.{1,50})\\\",\\\""
-        YouTubeChannelNameRegex2 = r"\"ownerChannelName\\\":\\\"(.{1,50})\\\","
-        YouTubeChannelNameRegex3 = r"Unsubscribe from ([^<{]*?)\?"
-        YouTubeChannelNameRegex4 = r"\"ownerChannelName\":\"(.*?)\","
+    try:
+        YouTubeChannelName  = re.search(YouTubeChannelNameRegex1, source_code).group(1)
+    except AttributeError:
         try:
-            YouTubeChannelName  = re.search(YouTubeChannelNameRegex1, source_code).group(1)
+            YouTubeChannelName  = re.search(YouTubeChannelNameRegex2, source_code).group(1)
         except AttributeError:
             try:
-                YouTubeChannelName  = re.search(YouTubeChannelNameRegex2, source_code).group(1)
+                YouTubeChannelName  = re.search(YouTubeChannelNameRegex3, source_code).group(1)
             except AttributeError:
                 try:
-                    YouTubeChannelName  = re.search(YouTubeChannelNameRegex3, source_code).group(1)
+                    YouTubeChannelName  = re.search(YouTubeChannelNameRegex4, source_code).group(1)
                 except AttributeError:
                     try:
-                        YouTubeChannelName  = re.search(YouTubeChannelNameRegex4, source_code).group(1)
+                        YouTubeChannelName  = re.search(YouTubeChannelNameRegex5, source_code).group(1)
                     except AttributeError:
-                        YouTubeChannelName = None
+                        try:
+                            YouTubeChannelName  = re.search(YouTubeChannelNameRegex6, source_code).group(1)
+                        except AttributeError:
+                            YouTubeChannelName = None
+    return YouTubeChannelName
 
-        match = re.search(r"videoViewCountRenderer\":{\"viewCount\":{\"simpleText\":\"([0-9,]*?) views\"},\"shortViewCount\":{\"simpleText\"", source_code)
-        if match:
-            view_count = match.group(1)
-        else:
-             view_count = None
+def get_youtube_view_count(source_code):
+    match = re.search(r"videoViewCountRenderer\":{\"viewCount\":{\"simpleText\":\"([0-9,]*?) views\"},\"shortViewCount\":{\"simpleText\"", source_code)
+    if match:
+        view_count = match.group(1)
+    else:
+         view_count = None
+    return view_count
 
+def get_license(source_code):
+    if re.search(r"Creative Commons", source_code):
+        license = "Creative Commons Attribution license (reuse allowed)"
+    else:
+        license = "Standard YouTube License"
+    return license
 
-        if re.search(r"Creative Commons", source_code):
-            license = "Creative Commons Attribution license (reuse allowed)"
-        else:
-            license = "Standard YouTube License"
+def get_duration(source_code):
+    match = re.search(r"approxDurationMs\\\":\\\"([0-9]*?)\\\"", source_code)
+    if match:
+        duration = format_time(match.group(1))
+    else:
+        duration = None
+    return duration
 
+def get_youtube_video_title(source_code):
+    YouTubeVideoTitleRegex1 = r"<title>(?:\s*|)(.{1,250})(?:\s*|)- YouTube(?:\s*|)</title>"
+    YouTubeVideoTitleRegex2 = r"\"title\":\"(.{1,160})\",\"length"
 
-        match = re.search(r"approxDurationMs\\\":\\\"([0-9]*?)\\\"", source_code)
-        if match:
-            duration = format_time(match.group(1))
-        else:
-            duration = None
-
-        thumbnail = "https://img.youtube.com/vi/%s/maxresdefault.jpg" % video_id
-
-
-        YouTubeVideoTitleRegex1 = r"<title>(?:\s*|)(.{1,250})(?:\s*|)- YouTube(?:\s*|)</title>"
-        YouTubeVideoTitleRegex2 = r"\"title\":\"(.{1,160})\",\"length"
+    try:
+        YouTubeVideoTitle   = clean_html(re.search(YouTubeVideoTitleRegex1, source_code).group(1))
+    except AttributeError:
         try:
-            YouTubeVideoTitle   = clean_html(re.search(YouTubeVideoTitleRegex1, source_code).group(1))
+            YouTubeVideoTitle   = clean_html(re.search(YouTubeVideoTitleRegex2, source_code).group(1))
         except AttributeError:
-            try:
-                YouTubeVideoTitle   = clean_html(re.search(YouTubeVideoTitleRegex2, source_code).group(1))
-            except AttributeError:
-                YouTubeVideoTitle = None
+            YouTubeVideoTitle = None
+
+    return YouTubeVideoTitle
+
+def ytdata(video_id, user_agent):
+    url = "https://www.youtube.com/watch?v=%s" % video_id
+    archive_url = str(get_archive(url, user_agent))
+
+    if archive_url:
+        response = requests.get(archive_url)
+        source_code = response.text
+
+        if not source_code:
+            response = requests.get(url)
+            source_code = response.text
+
+        upload_date = get_upload_date(source_code)
+        video_description = get_video_description(source_code)
+        YouTubeChannelId = get_youtube_channel_id(source_code)
+        YouTubeChannelName = get_youtube_channel_name(source_code)
+        view_count = get_youtube_view_count(source_code)
+        thumbnail = "https://img.youtube.com/vi/%s/maxresdefault.jpg" % video_id
+        license = get_license(source_code)
+        duration = get_duration(source_code)
+        YouTubeVideoTitle = get_youtube_video_title(source_code)
 
         return (url, user_agent, archive_url, upload_date, video_description, YouTubeChannelId, YouTubeChannelName, YouTubeVideoTitle, license, view_count, duration, thumbnail)
 
